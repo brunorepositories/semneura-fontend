6e290b1377d7557777cc18afb0046c2be8dcf474
Bruno Henrique <brunohs.dev@gmail.com>
2019-04-05 22:04:52 -0300

    ajuste

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue);
+
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  render: r => r(App)
 })
